9cbef3f8b50faa74011614c44461e71d84ba5614
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-09-18 22:36:17 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Sticker▪react.js
+++ plugins/Sticker▪react.js
@@ -1,9 +1,9 @@
 let { proto } = (await import('@adiwajshing/baileys')).default
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
   if (!args[0]) throw `⚠️️ *_Responde a un mensaje usando el comando más un emoji para reaccionar._*\n\n📌 *_Ejemplo_* ${usedPrefix + command} 🤫`
   let q = m.quoted ? await m.getQuotedObj() : m
-  conn.relayMessage(m.chat, { reactionMessage: proto.ReactionMessage.create({ key: q.key, text: args[0] }) }, { messageId: q.key.id })
+ conn.sendMessage(m.chat, { react: { text: args[0], key: q.key }}, { quoted: q })
 }
 handler.help = ['react *<emoji>*']
 handler.tags = ['sticker']
