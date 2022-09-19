bac229ac0644245eaf55c801101b0fbfa074a5fe
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-09-18 21:47:28 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

---
+++ plugins/Owner▪delprem.js
@@ -0,0 +1,17 @@
+let handler = async (m, { conn, text }) => {
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+if (!who) throw `*_⚠️ Ingrese el @tag de la persona a la que desea quitarle premium._*`
+if (!global.prems.includes(who.split`@`[0])) throw '*_⚠ ️El usuario que ingresó no es un usuario premium._*'
+let index = global.prems.findIndex(v => (v.replace(/[^0-9]/g, '') + '@s.whatsapp.net') === (who.replace(/[^0-9]/g, '') + '@s.whatsapp.net'))
+global.prems.splice(index, 1)
+let textdelprem = `*_•️ Lo siento @${who.split`@`[0]} dejaste de formar parte de los usuarios premium._*`
+m.reply(textdelprem, null, { mentions: conn.parseMention(textdelprem) })
+}
+handler.help = ['delprem <@user>']
+handler.tags = ['owner']
+handler.command = /^(remove|-|del)prem$/i
+handler.group = true
+handler.rowner = true
+export default handler
