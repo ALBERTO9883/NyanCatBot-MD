b3e4fe1a520c2a3e7b1d0b2394579d1df2f117a3
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-09-21 15:47:46 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Info▪reportar.js
+++ plugins/Info▪reportar.js
@@ -3,11 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (text.length < 10) throw '⚠️ *_Especifique bien el error, mínimo 10 caracteres._*'
     if (text.length > 1000) throw '⚠️ *_Máximo 1000 caracteres para enviar el error._*'
     let teks = `*[ REPORTE ]*\n\n*• Usuario:* @${m.sender.split`@`[0]}\n*• Texto:* ${text}`
-    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, {
-        contextInfo: {
-            mentionedJid: [m.sender]
-        }
-    })
+    await conn.reply(global.owner[0][0] + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, m, { mentions: conn.parseMention(teks) })
     m.reply('⚠️ *_El reporte se envío a mi creador, cualquier informe falso puede ocasionar baneo._*')
 }
 handler.help = ['reportar']
