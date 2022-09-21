cba99d1d5f808b681300a0b31b18fb81bc9ca008
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-09-21 16:07:16 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Owner▪banuser.js
+++ plugins/Owner▪banuser.js
@@ -6,7 +6,7 @@ else who = m.chat
 if (!who) throw '⚠️ *_Ingresa el @tag de algún usuario._*'
 let users = global.db.data.users
 users[who].banned = true
-conn.reply(m.chat, `⚠️ *_El usuario fue baneado con éxito._*`, m)
+conn.reply(m.chat, `⚠️ *_El usuario @${user.split('@')[0]}, fue baneado con éxito._*`, m, { mentions: [user]})
 }
 handler.help = ['banuser']
 handler.tags = ['owner']
