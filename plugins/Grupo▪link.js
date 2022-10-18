9c4cc017146e4fa1aa983f09ec62066c97f69670
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-10-18 12:03:27 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Grupo▪link.js
+++ plugins/Grupo▪link.js
@@ -1,8 +1,10 @@
 import { areJidsSameUser } from '@adiwajshing/baileys'
+import fetch from 'node-fetch' 
 let handler = async (m, { conn, args }) => {
     let group = m.chat
     if (/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(args[0])) group = args[0]
     if (!/^[0-9]{5,16}-?[0-9]+@g\.us$/.test(group)) throw '⚠️ Sólo se puede usar en grupos'
+    let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
     let groupMetadata = await conn.groupMetadata(group)
     if (!groupMetadata) throw 'groupMetadata is undefined :\\'
     if (!('participants' in groupMetadata)) throw 'participants no está definido :('
@@ -10,7 +12,7 @@ let handler = async (m, { conn, args }) => {
     if (!me) throw '✳️ No estoy en ese grupo :('
     if (!me.admin) throw '✳️ No soy administrador'
     await conn.reply(m.chat, global.wait, m)
-    await conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), fakemsg)
+    await conn.sendNyanCat(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), await (await fetch(pp)).buffer(), `${groupMetadata.subject}`, null, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m)
 }
 handler.help = ['Link']
 handler.tags = ['group']
