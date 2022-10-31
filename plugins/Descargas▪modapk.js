18dbbff01f488d3c3452c2e7102f113f0950bfbf
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2022-10-31 14:01:19 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

---
+++ plugins/Descargas▪modapk.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch' 
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `⚠️️ *_Ingrese el link de modlatest junto al comando_*`
+    if (!args[0].match(/rexdl.com/gi)) throw `❎ Link incorrecto`
+   
+    let res = await fetch(`https://api.akuari.my.id/downloader/dlmod2?link=${args[0]}`)
+    let json = await res.json()
+    let { title, version, size, updated } = json.respon
+    let info = `*🐢 • Título:* ${title}\n*🎋 • Versión:* ${version}\n*🗓️ • Actualización:* ${updated}\n*📁 • Tamaño:* ${size}`
+    await conn.sendNyanCat(m.chat, `${info}\n${global.wait}`, adnyancat, botname, me, script, m)
+    
+conn.sendMessage(m.chat, { document: { url: `${json.respon.download[0].url}` }, mimetype: 'application/videos.android.package-archive', fileName: `${title}` }, { quoted: m })
+}
+handler.help = ['dlmodapk *<url>*']
+handler.tags = ['downloader']
+handler.command = ['dlmodapk', 'dlapkmod'] 
+handler.register = true
+
+handler.limit = true
+
+export default handler
