ca63b8d3e16d119865f0057ed3b83e1311c50eff
SinNombre999 <100889859+SinNombre999@users.noreply.github.com>
2022-11-17 22:35:53 -0600

    Update Tools▪translate.js

--- plugins/Tools▪translate.js
+++ plugins/Tools▪translate.js
@@ -1,45 +1,32 @@
 import translate from 'translate-google-api'
-const defaultLang = 'es'
-const tld = 'cn'
-
+import fetch from 'node-fetch'
 let handler = async (m, { args, usedPrefix, command }) => {
-    let err = `
+let msg =  `
 📌 *_Ejemplo :_*
 *${usedPrefix + command}* <idioma> [texto]
 ${usedPrefix + command} es Hello World
-
 ✳️ Lista de idiomas admitidos: 
-
 https://cloud.google.com/translate/docs/languages
 `.trim()
-
-    let lang = args[0]
-    let text = args.slice(1).join(' ')
-    if ((args[0] || '').length !== 2) {
-        lang = defaultLang
-        text = args.join(' ')
-    }
-    if (!text && m.quoted && m.quoted.text) text = m.quoted.text
-
-    let result
-    try {
-        result = await translate(`${text}`, {
-            tld,
-            to: lang,
-        })
-    } catch (e) {
-        result = await translate(`${text}`, {
-            tld,
-            to: defaultLang,
-        })
-        throw err
-    } finally {
-        m.reply(result[0])
-    }
-
-}
-handler.help = ['translate *<idioma> <texto>*']
-handler.tags = ['tools']
-handler.command = ['translate', 'tl', 'trad', 'tr']
-
+if (!args || !args[0]) return m.reply(msg)  
+let lang = args[0]
+let text = args.slice(1).join(' ')
+const defaultLang = 'es'
+if ((args[0] || '').length !== 2) {
+lang = defaultLang
+text = args.join(' ')}
+if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+try {      
+let result = await translate(`${text}`, { tld, to: lang, })
+await m.reply('' + result)
+} catch {
+try {    
+let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
+let loll = await lol.json()
+let result2 = loll.result.translated
+await m.reply(''+ result2)
+} catch { 
+await m.reply('*_Error inesperado, vuelva a intentarlo más tarde :)_*')    
+}}}
+handler.command = /^(translate|traducir|trad)$/i
 export default handler
