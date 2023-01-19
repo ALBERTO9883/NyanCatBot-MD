6ddd0189f6197871f7d7deb2737b25d2183787d4
SinNombre999 <100889859+SinNombre999@users.noreply.github.com>
2023-01-19 10:21:29 -0600

    Update Descargas▪facebook.js

--- plugins/Descargas▪facebook.js
+++ plugins/Descargas▪facebook.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, command, text, args }) => {
 if (command == 'facebook') {
 if (!text) throw `⚠️ *_Ingrese el link o enlace de Facebook._*`
 await conn.sendNyanCat(m.chat, global.wait, adnyancat, addescargas, null, script, m)
-let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=85faf717d0545d14074659ad&url=${text}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=BrunoSobrino&url=${text}`)
 let json = await res.json()
 let { result } = json
 conn.sendFile(m.chat, result, 'error.mp4', `*Aquí tiene su video de Facebook. ฅ^•ﻌ•^ฅ⚘*`, m)}
 if (command == 'facebook2') {
 if (!text) throw `⚠️ *_Ingrese el link o enlace de Facebook._*`
 await conn.sendNyanCat(m.chat, global.wait, adnyancat, addescargas, null, script, m)
-let res = await fetch(`https://api.lolhuman.xyz/api/facebook2?apikey=85faf717d0545d14074659ad&url=${text}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/facebook2?apikey=BrunoSobrino&url=${text}`)
 let json = await res.json()
 let { result } = json
 conn.sendFile(m.chat, result, 'error.mp4', `*Aquí tiene su video de Facebook. ฅ^•ﻌ•^ฅ⚘*`, m)}
