fc1080288c229751079913a61ceab33bf47347cc
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-01-29 19:51:21 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/YouTube▪playdoc.js
+++ plugins/YouTube▪playdoc.js
@@ -1,25 +1,25 @@
-import { youtubeSearch } from '@bochilteam/scraper'
+import yts from 'yt-search'
 import fs from 'fs'
 import fetch from 'node-fetch' 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `⚠️ *_Ingresa el título de una canción._*\n\n*_📌 Ejemplo :_* *${usedPrefix + command}* Sit Down Be Side Me`
-let vid = (await youtubeSearch(text)).video[0]
-if (!vid) throw '⚠️️ *_Vídeo/Audio no encontrado :(._*'
+	if (!text) throw `⚠️ *_Ingresa el título de una canción._*\n\n*_📌 Ejemplo :_* *${usedPrefix + command}* Sit Down Be Side Me`
+	let vid = (await yts(text)).all[0]
+	if (!vid) throw '⚠️️ *_Vídeo/Audio no encontrado :(._*'
 try {
-let { authorName, title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-const url = 'https://www.youtube.com/watch?v=' + videoId
+let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]
 const buttons = [
-{ buttonId: `.yta.2 ${url}`, buttonText: { displayText: 'Aᴜᴅɪᴏ 🎵' }, type: 1 },
-{ buttonId: `.ytv.2 ${url}`, buttonText: { displayText: 'Vɪᴅᴇᴏ 🎥' }, type: 1 }, ]    
+{ buttonId: `.yta ${url}`, buttonText: { displayText: 'Aᴜᴅɪᴏ 🎵' }, type: 1 },
+{ buttonId: `.ytv ${url}`, buttonText: { displayText: 'Vɪᴅᴇᴏ 🎥' }, type: 1 },
+{ buttonId: `.ytgetdl ${url}`, buttonText: { displayText: 'Cᴀʟɪᴅᴀᴅᴇs📁' }, type: 1 }, ]    
 let texto1 = `*⊜─⌈📻 ◜YouTube Doc.◞ 📻⌋─⊜*
 
-❏ 🐢 *Autor:* ${authorName}
+❏ 🐢 *Autor:* ${vid.author.name}
 ❏ 📓 *Título:* ${title}
-❏ 🗓️ *Publicado:* ${publishedTime}
-❏ ⏰ *Duración:* ${durationH}
-❏ 👀 *Vistas:* ${viewH}
+❏ 🗓️ *Publicado:* ${ago}
+❏ ⏰ *Duración:* ${timestamp}
+❏ 👀 *Vistas:* ${views}
 `
 let buttonMessage = {
 "document": { url: "https://wa.me/50499698072" }, 
@@ -44,7 +44,8 @@ contextInfo: {
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }catch(e){
 m.reply('⚠️ *_Error, porfavor vuelva a intentarlo._*')
-console.log(e)
-}}
+console.log(e)}}
+handler.help = ['playdoc *<song name>*']
+handler.tags = ['downloader']
 handler.command = /^play3|playdoc?$/i
 export default handler
