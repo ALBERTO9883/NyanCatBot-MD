aab9574e7375a106a1468e49a4e7b3e0bdb56733
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-01-29 19:28:48 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Tools▪translate.js
+++ plugins/Tools▪translate.js
@@ -1,13 +1,7 @@
-import translate from 'translate-google-api'
+import translate from '@vitalets/google-translate-api'
 import fetch from 'node-fetch'
 let handler = async (m, { args, usedPrefix, command }) => {
-let msg =  `
-📌 *_Ejemplo :_*
-*${usedPrefix + command}* <idioma> [texto]
-${usedPrefix + command} es Hello World
-✳️ Lista de idiomas admitidos: 
-https://cloud.google.com/translate/docs/languages
-`.trim()
+let msg = `*⚠️ Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*📌 Ejemplo:*\n*${usedPrefix + command} es Hi brother*\n\n*Lista de idiomas admitidos:*\n*- https://cloud.google.com/translate/docs/languages*`
 if (!args || !args[0]) return m.reply(msg)  
 let lang = args[0]
 let text = args.slice(1).join(' ')
@@ -17,18 +11,16 @@ lang = defaultLang
 text = args.join(' ')}
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 try {      
-let result = await translate(`${text}`, { tld, to: lang, })
-await m.reply('' + result)
+let result = await translate(`${text}`, { to: lang, autoCorrect: true })
+await m.reply('*Traducción:* ' + result.text)
 } catch {
 try {    
-let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
+let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`)
 let loll = await lol.json()
 let result2 = loll.result.translated
-await m.reply(''+ result2)
+await m.reply('*Traducción:* ' + result2)
 } catch { 
-await m.reply('*_Error inesperado, vuelva a intentarlo más tarde :)_*')    
+await m.reply('*⚠️ ERROR, VUELVA A INTENTARLO*')    
 }}}
-handler.tags = ['tools']
-handler.help = ['translate <idioma> <texto>']
 handler.command = /^(translate|traducir|trad)$/i
 export default handler
