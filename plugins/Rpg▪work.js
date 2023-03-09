8e5b709b94050b8f5248b6351d1d53a685586d5e
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-03-09 16:58:13 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Rpg▪work.js
+++ plugins/Rpg▪work.js
@@ -1,15 +1,15 @@
-import db from '../lib/database.js'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, isPrems}) => {
 
 
   let hasil = Math.floor(Math.random() * 5000)
   let time = global.db.data.users[m.sender].lastwork + 600000
-  if (new Date - global.db.data.users[m.sender].lastwork < 600000) throw `*Estas cansado* y por lo tanto hay que esperar ${msToTime(time - new Date())} para volver a trabajar!`
-
-  m.reply(`
-‣ ${pickRandom(global.work)} *${hasil} XP*
-`)
+  if (new Date - global.db.data.users[m.sender].lastwork < 600000) { conn.sendNyanCat(m.chat, `*Estas cansado* y por lo tanto hay que esperar ${msToTime(time - new Date())} para volver a trabajar!`, await (await fetch('https://telegra.ph/file/0056079ce61eb0cbf5768.jpg')).buffer(), '🐱*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD ❐🎋༉', `Tᴏᴍᴀ́ɴᴅᴏsᴇ ᴜɴ ᴄᴀғᴇᴄɪᴛᴏ☕`, script, m)
+  throw false }
+  let img = `${pickRandom(global.workimg)}`
+  let imgwork = await (await fetch(img)).buffer()
+await conn.sendNyanCat(m.chat, `‣ ${pickRandom(global.work)} *${hasil} XP*`, imgwork, '🐱*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD ❐🎋༉', `Wᴏʀᴋ ʙʏ NʏᴀɴCᴀᴛBᴏᴛ👷🏻`, script, m)
   global.db.data.users[m.sender].lastwork = new Date * 1
 }
 handler.help = ['work']
@@ -39,6 +39,7 @@ function msToTime(duration) {
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
+global.workimg = ["https://telegra.ph/file/2f808fabe96c12ca51ba1.jpg", "https://telegra.ph/file/6780684c8a80cbb3e8d76.jpg", "https://telegra.ph/file/03a619ca25e9d7a2f13c7.jpg", "https://telegra.ph/file/3941ff6c7712e42756c7b.jpg", "https://telegra.ph/file/47207304b6f6db3461675.jpg", "https://telegra.ph/file/d954848cd9332b1dd5c26.jpg"]
 
 global.work = ["Trabajas como cortador de galletas y ganas", "Trabaja para una empresa militar privada, ganando", "Organiza un evento de cata de vinos y obtiene",
  "Te secuestran y te llevan a un coliseo subterráneo donde luchaste contra monstruos con personas que nunca antes habías conocido. Ganas", "Limpias la chimenea y encuentras", 
