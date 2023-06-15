b3d2e8b7e7fa1581ad556210bcb9fd7758b8d810
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-06-15 13:22:10 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Img▪wallpaper2.js
+++ plugins/Img▪wallpaper2.js
@@ -1,12 +1,12 @@
-import { wallpaper, wallpaperv2 } from '@bochilteam/scraper'
+import { wallpaper } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) throw `Harap masukkan query wallpaper yang ingin dicari.\n\nContoh: ${usedPrefix + command} Bronya Zaychik Honkai`
-    const res = await (/2/.test(command) ? wallpaperv2 : wallpaper)(text)
+    if (!text) throw `*_⚠️ Ingrese el nombre de lo que desea buscar._*\n\n*• Ejemplo:* ${usedPrefix + command} Minecraft`
+    const res = await (/2/.test(command) ? wallpaper : wallpaper)(text)
     const img = res[Math.floor(Math.random() * res.length)]
     await conn.sendNyanCat(m.chat, global.wait, adnyancat, adimagen, null, script, m)
     conn.sendFile(m.chat, img, 'wallpaper.jpg', wm, m)
 }
-handler.help = ['', '2'].map(v => 'wallpaper' + v + ' *<texto>*')
+handler.help = ['wallpaper2']
 handler.tags = ['downloader']
 
 handler.command = /^(wallpaper2?)$/i
