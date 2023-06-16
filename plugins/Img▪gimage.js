88ba1f5b0e155c48aa2c336403051bc1ce2f4fd6
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-06-16 12:53:09 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Img▪gimage.js
+++ plugins/Img▪gimage.js
@@ -3,8 +3,8 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `⚠️️ *_Que imagen busco?_*`
     const res = await googleImage(text)
     await conn.sendNyanCat(m.chat, global.wait, adnyancat, adimagen, null, script, m)
- 
-conn.sendButton(m.chat, `✅ Resultado de : *${text}*`, '• Click en siguiente para ir a la siguiente imagen', res.getRandom(), [['「🔃 Sɪɢᴜɪᴇɴᴛᴇ 🔃」', `${usedPrefix + command} ${text}`]], fakemsg)
+
+ conn.sendFile(m.chat, res.getRandom(), 'error.jpg', `✅ *Resultado de:* ${text}`, fakemsg)
 }
 handler.help = ['imagen *<texto>*']
 handler.tags = ['img']
