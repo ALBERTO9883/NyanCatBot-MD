2a2194cdc809d51e677ce0f56bbf67c1522cef7b
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-06-25 00:01:29 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Tools▪chatgpt.js
+++ plugins/Tools▪chatgpt.js
@@ -1,25 +1,25 @@
-import fetch from 'node-fetch' 
- let handler = async (m, { text, usedPrefix, command }) => { 
- if (!text) throw `*_🪻 • Ingrese su petición._*\n*🪼 Ejemplo de uso:* ${usedPrefix + command} como hacer un gatito con papel` 
- m.ftext(wait)
- try { 
- let IA2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)   
- let IAR2 = await IA2.json() 
- m.ftext(`${IAR2.response}`.trim())     
- } catch { 
- try {    
- let rrEes = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`) 
- let jjJson = await rrEes.json() 
- m.ftext(jjJson.data.data.trim())     
- } catch {       
- try {     
- let tioress = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=BrunoSobrino&text=${text}&user=${m.sender}`) 
- let hasill = await tioress.json() 
- m.ftext(`${hasill.result}`.trim())    
- } catch {         
- throw `*_⚠️ Error, inténtelo más tarde._*` 
- }}}} 
- handler.help = ['openai *<petición>*', 'ia *<petición>*']
- handler.tags = ['tools']
- handler.command = ['openai', 'chatgpt', 'ia', 'robot', 'ai'] 
- export default handler
+import fetch from 'node-fetch'
+import fg from 'api-dylux'
+
+let handler  =  async  (m, { text,  usedPrefix,  command 
+})  => {
+    if  (!text) 
+    throw ` *_🪻•Ingrese su petición._* \n *🪼Ejemplo de uso:* ${usedPrefix  +  command } como hacer un gatito con papel`
+    m.ftext(wait)
+    try  {
+        let jsons = await fg.ChatGpt(`${text}`)
+        m.ftext(jsons.text)    
+    } 
+    catch  {        
+        throw `*_⚠️Error, inténtelo más tarde._*`
+    }
+}
+handler.help = ['openai *<petición>*', 'ia *<petición>*']
+handler.tags = ['tools']
+handler.command  =   ['openai',  
+'chatgpt',  
+'ia',  
+'robot', 'ai']
+export default handler
+
+handler.register = true
