b7ef42a4a0cedd73ee67cdb6c7018b05d0e1abba
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-06-25 18:04:44 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Reg▪sn.js
+++ plugins/Reg▪sn.js
@@ -2,14 +2,8 @@ import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-let nserie = `
- *🌅 | • _NÚMERO DE SERIE_ •*
-
-*► ${sn}*`
-const botones = [
-{index: 1, urlButton: {displayText: `•Cᴏᴘɪᴀʀ Nsᴇʀɪᴇ`, url: `https://www.whatsapp.com/otp/copy/${sn}`}},
-]
-await conn.sendMessage(m.chat, { text: `${nserie}`, templateButtons: botones, footer: botname })
+await m.reply(`*🌅 | • _SU NÚMERO DE SERIE_ •* ⬇️`)
+await m.reply(`${sn}`.trim())
 }
 handler.help = ['nserie']
 handler.tags = ['rg']
