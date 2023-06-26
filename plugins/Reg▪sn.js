81542e69aa8e6796dc5cff8b7aaac9a4e458851a
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-06-25 21:09:29 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Reg▪sn.js
+++ plugins/Reg▪sn.js
@@ -2,8 +2,8 @@ import { createHash } from 'crypto'
 
 let handler = async function (m, { conn, text, usedPrefix }) {
 let sn = createHash('md5').update(m.sender).digest('hex')
-await m.reply(`*🌅 | • _SU NÚMERO DE SERIE_ •* ⬇️`)
-await m.reply(`${sn}`.trim())
+await m.ftext(`*🌅 | • _SU NÚMERO DE SERIE_ •* ⬇️`)
+await m.ftext(`${sn}`.trim())
 }
 handler.help = ['nserie']
 handler.tags = ['rg']
