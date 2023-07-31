8dcb3e23e7fcc78ca37a1cab0da8f81016233317
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-07-31 13:17:02 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Buscador▪google.js
+++ plugins/Buscador▪google.js
@@ -3,6 +3,7 @@ let handler = async (m, { conn, command, args }) => {
     const fetch = (await import('node-fetch')).default
     let full = /f$/i.test(command)
     let text = args.join` `
+    let img = await (await fetch('https://telegra.ph/file/06ea3fdb002f06261fbb1.jpg')).buffer()
     if (!text) return conn.reply(m.chat, '*_🔎 Que quieres buscar en Google?_*', m)
     let url = 'https://google.com/search?q=' + encodeURIComponent(text)
     let search = await googleIt(text)
@@ -19,7 +20,7 @@ let handler = async (m, { conn, command, args }) => {
         if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
         await conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
     } catch (e) {
-        m.reply(msg)
+        conn.sendNyanCat(m.chat, msg, img, '• Resultados Encontrados🔎', botname, script, m)
     }
 }
 handler.help = ['google *<buscar>*']
