aa70865f68dc3bb6019d543be7599525047aafa3
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-07-31 15:12:24 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Buscador▪yts.js
+++ plugins/Buscador▪yts.js
@@ -1,12 +1,14 @@
 
 import yts from 'yt-search'
 import fs from 'fs'
+import fetch from 'node-fetch'
 
 let handler = async (m, {conn, text }) => {
   if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*'
   await conn.reply(m.chat, global.wait, m)
   let results = await yts(text)
   let tes = results.all
+  let img = await (await fetch(`${tes[0].thumbnail}`)).buffer()
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
@@ -15,7 +17,14 @@ let handler = async (m, {conn, text }) => {
 ↳ 🕒 *_Duración :_* ${v.timestamp}
 ↳ 📥 *_Subido :_* ${v.ago}
 ↳ 👁 *_Vistas :_* ${v.views}`}}).filter(v => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
-  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+  await conn.sendUrl(m.chat, teks, m, {
+    externalAdReply: {
+       mediaType: 1,
+       renderLargerThumbnail: true,
+       thumbnail: img,
+       thumbnailUrl: img,
+       title: botname,
+    }})
 }
 handler.help = ['ytsearch *<texto>*'] 
 handler.tags = ['search']
