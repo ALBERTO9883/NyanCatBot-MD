40d1d34b5129003da6d0a47ea63502d1cb652119
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-07-31 12:52:29 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Anime▪lolivid.js
+++
@@ -1,9 +0,0 @@
-let handler  = async (m, { conn, usedPrefix, command }) => {
-let res = "https://api.zacros.my.id/asupan/loli"
-await conn.sendNyanCat(m.chat, global.wait, adnyancat, adanime, null, script, m)
-conn.sendHydrated(m.chat, null, null, res, null, null, null, null, [['「🔃 Sɪɢᴜɪᴇɴᴛᴇ 🔃」', `/${command}`]], m)}
-handler.help = ['lolivid']
-handler.tags = ['nime']
-handler.command = /^(lolivid|lolivideos|lolívid)$/i
-export default handler
-handler.register = true
