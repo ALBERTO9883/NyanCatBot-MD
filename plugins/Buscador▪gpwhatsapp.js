1c43ec033f4f862c5c4f41096bdbaad7f12c242c
Alberto Acosta <100887441+ALBERTO9883@users.noreply.github.com>
2023-07-31 13:58:41 -0600

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Buscador▪gpwhatsapp.js
+++ plugins/Buscador▪gpwhatsapp.js
@@ -1,21 +1,22 @@
-import { googleImage } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 import fs from 'fs'
 let handler = async (m, {text, usedPrefix, command, conn}) => {
 if (!text) throw `*⚠️ Ingrese el nombre de la apk o app que desea buscar.*`
-const res = await googleImage(text)
-let image = res.getRandom()
-let link = image
 
-let json = await fetch(`https://dhn-api.herokuapp.com/api/search/whatsapp-group?query=${text}&apikey=4709b3d201416e985351`)
+let img = await (await fetch('https://telegra.ph/file/2df89bd87eae9e3173635.jpg')).buffer()
+let json = await fetch(`https://api.lolhuman.xyz/api/groupwhatsapp?apikey=${lolkeysapi}&query=${text}`)
 let jsons = await json.json()
-let caption = `*⊜ RESULTADOS🔎*\n`
-for (let x of jsons.result) {
-caption += `
-*• 🏷 Nombre:* ${x.data_title}
-*• 🔗 Link:* _${x.data_url}_\n───
-`}
-await conn.reply(m.chat, caption, m)}
+let res = Object.values(jsons.result).map(v => v)
+if (!res.length) return m.reply('⚠️ *_No se encontraron resultados, intente con otro nombre._*').then(_ => m.react('✖️'))
+let txt = `*⊜ RESULTADOS🔎*`
+for (let i = 0; i < (15 <= res.length ? 15 : res.length); i++) {
+   txt += `\n\n`
+   txt += `	⤿  *Nro* : ${1+i}\n`
+   txt += `	⤿  *Nombre* : ${res[i].title}\n`
+   txt += `	⤿  *Link* : ${res[i].link}\n`
+}
+conn.sendNyanCat(m.chat, txt, img, botname, null, script, m)
+}
 handler.tags = ['search']
 handler.command = handler.help = ['groupsearch']
 handler.register = true
