18d2b2e13801ff4aa3fcda42250c7ff2e0fa3d03
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-08-01 00:05:35 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/YouTube▪play.js
+++ plugins/YouTube▪play.js
@@ -7,16 +7,34 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 	if (!vid) throw '⚠️️ *_Vídeo/Audio no encontrado :(._*'
 try {
 let { title, description, thumbnail, videoId, timestamp, views, ago, url } = vid    
-let texto1 = `*⊜─⌈📻 ◜YouTube Play◞ 📻⌋─⊜*
-
+let texto1 = `
 ❏ 🐢 *Autor:* ${vid.author.name}
 ❏ 📓 *Título:* ${title}
 ❏ 🗓️ *Publicado:* ${ago}
 ❏ ⏰ *Duración:* ${timestamp}
 ❏ 👀 *Vistas:* ${views}
-❏ 🌱 *Url:* ${url}
-`
-conn.sendFile(m.chat, thumbnail, 'error.jpg', texto1, m)
+❏ 🌱 *Url:* ${url}`
+let buttonMessage= {
+'document': { url: `https://github.com/ALBERTO9883` },
+'mimetype': `application/pdf`,
+'fileName': `⊜─⌈📻 ◜YouTube Play◞ 📻⌋─⊜`,
+'fileLength': 99999999999999,
+'pageCount': 200,
+'contextInfo': {
+'forwardingScore': 200,
+'isForwarded': true,
+'externalAdReply': {
+'mediaUrl': `${url}`,
+'mediaType': 2,
+'previewType': 'VIDEO',
+'title': null,
+'body': null,
+'thumbnail': await (await fetch(thumbnail)).buffer(),
+'sourceUrl': 'https://youtube.com/' }},
+'caption': texto1,
+'footer': '\nVideos de YouTube',
+'headerType': 6 }
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
 }catch(e){
 m.reply('⚠️ *_Error, porfavor vuelva a intentarlo._*')
 console.log(e)}}
