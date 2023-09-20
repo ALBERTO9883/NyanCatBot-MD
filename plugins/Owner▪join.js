900175055119c0590407b4586dbda04fdbf88e83
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-20 09:43:27 -0600

    Update Owner▪join.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/Owner▪join.js
+++ plugins/Owner▪join.js
@@ -3,12 +3,12 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 	
-	if (!text) throw `⚠️️ *_Envie el link de tu Grupo_*\n\n📌 *_Ejemplo_* *${usedPrefix + command}* https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU`
+	if (!text) throw `[🚨] 𝐏𝐫𝐢𝐦𝐞𝐫𝐨 𝐞𝐧𝐯𝐢𝐚 𝐞𝐥 𝐥𝐢𝐧𝐤 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐨\n\n📌 𝐄𝐣𝐞𝐦𝐩𝐥𝐨 *${usedPrefix + command}* https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU`
     let [_, code, expired] = text.match(linkRegex) || []
-    if (!code) throw '⚠️Link invalido'
+    if (!code) throw '[🚨] 𝐄𝐫𝐫𝐨𝐫, 𝐄𝐬𝐭𝐞 𝐥𝐢𝐧𝐤 𝐞𝐬 𝐢𝐧𝐯𝐚𝐥𝐢𝐝𝐨 𝐨 𝐧𝐨 𝐟𝐮𝐞 𝐫𝐞𝐜𝐨𝐧𝐨𝐜𝐢𝐝𝐨'
     let res = await conn.groupAcceptInvite(code)
     expired = Math.floor(Math.min(999, Math.max(1, isOwner ? isNumber(expired) ? parseInt(expired) : 0 : 3)))
-    m.reply(`*_✅ Me uní correctamente al grupo ${res}${expired ? ` Durante ${expired} días_*` : ''}`)
+    m.reply(`[🐺] 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨, 𝐋𝐨𝐛𝐨-𝐁𝐨𝐭-𝐌𝐃 𝐬𝐞 𝐮𝐧𝐢𝐨 𝐚𝐥 𝐠𝐫𝐮𝐩𝐨 ${res}${expired ? ` 𝐃𝐮𝐫𝐚𝐧𝐭𝐞 ${expired} 𝐝𝐢𝐚𝐬_*` : ''}`)
     let chats = global.db.data.chats[res]
     if (!chats) chats = global.db.data.chats[res] = {}
     if (expired) chats.expired = +new Date() + expired * 1000 * 60 * 60 * 24
