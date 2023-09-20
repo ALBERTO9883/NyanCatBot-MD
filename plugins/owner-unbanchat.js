d788870f9d74dec7a7d9829fe6ddad20407ff952
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-20 09:49:17 -0600

    Update owner-unbanchat.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply('*_✅ Bot desmuteado en este chat, ahora el bot reaccionará a todos los comandos!_*')
+    m.reply('[➡️] 𝐏𝐞𝐫𝐟𝐞𝐜𝐭𝐨, 𝐄𝐬𝐭𝐞 𝐜𝐡𝐚𝐭 𝐲𝐚 𝐭𝐢𝐞𝐧𝐞 𝐩𝐞𝐫𝐦𝐢𝐬𝐨 𝐩𝐚𝐫𝐚 𝐮𝐬𝐚𝐫 𝐋𝐨𝐛𝐨-𝐁𝐨𝐭-𝐌𝐃')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
