f3b9815794e49a21c703f35e4489d00c2cfe94d8
EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>
2023-09-20 09:30:23 -0600

    Update Owner▪unbanuser.js
    Signed-off-by: EnderGB <142455051+Ender-GB-Isis777@users.noreply.github.com>

--- plugins/Owner▪unbanuser.js
+++ plugins/Owner▪unbanuser.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text}) => {
-if (!text) throw '⚠️ *_Ingresa el @tag de algún usuario._*'
+if (!text) throw '[⛔],𝐏𝐫𝐢𝐦𝐞𝐫𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚 𝐞𝐥 @tag 𝐝𝐞𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐚 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐫'
 let who
 if (m.isGroup) who = m.mentionedJid[0]
 else who = m.chat
-if (!who) throw '⚠️ *_Ingresa el @tag de algún usuario._*'
+if (!who) throw '[⛔],𝐏𝐫𝐢𝐦𝐞𝐫𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚 𝐞𝐥 @tag 𝐝𝐞𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 𝐚 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐫'
 let users = global.db.data.users
 users[who].banned = false
-conn.reply(m.chat, `⚠️ *_El usuario @${who.split('@')[0]} fue desbaneado con éxito._*`, fgclink, { mentions: [who] })
+conn.reply(m.chat, `[➡️] 𝐄𝐥 𝐮𝐬𝐮𝐚𝐫𝐢𝐨 @${who.split('@')[0]} 𝐅𝐮𝐞 𝐝𝐞𝐬𝐛𝐚𝐧𝐞𝐚𝐝𝐨 𝐜𝐨𝐧 𝐞𝐱𝐢𝐭𝐨, 𝐲𝐚 𝐩𝐮𝐞𝐝𝐞 𝐮𝐬𝐚𝐫 𝐋𝐨𝐛𝐨-𝐁𝐨𝐭-𝐌𝐃.`, fgclink, { mentions: [who] })
 }
 handler.help = ['unbanuser']
 handler.tags = ['owner']
