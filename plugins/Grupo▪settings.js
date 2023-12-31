a1ae03602a8e87d876d502660a6a6a59c23781d4
Yorman × <105564821+yorman-lopez@users.noreply.github.com>
2023-12-31 14:27:18 -0500

    🐱◌*̥₊ NʏᴀɴCᴀᴛBᴏᴛ - MD◌❐🎋༉

--- plugins/Grupo▪settings.js
+++ plugins/Grupo▪settings.js
@@ -2,16 +2,17 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = {
 		'open': 'not_announcement',
 		'on': 'not_announcement',
-		'1': 'not_announcement',
+		'abrir': 'not_announcement',
 		'close': 'announcement',
 		'off': 'announcement',
-		'0': 'announcement',
+		'cerrar': 'announcement',
 	}[(args[0] || '')]
-	if (isClose === undefined) {
-		await conn.sendButton(m.chat, `
-⚠️ *_Porfavor elija una opción._*`.trim(), botname, false, [['「ABRIR🔓」', '.grupo 1'], ['「CERRAR🔒」', '.grupo 0']], m)
-		throw false
-	}
+	if (isClose === undefined)
+		throw `
+⚠️ *_Porfavor elija una opción:_*
+  ${usedPrefix + command} cerrar
+  ${usedPrefix + command} abrir
+`.trim()
 	await conn.groupSettingUpdate(m.chat, isClose)
 }
 handler.help = ['grupo <abrir/cerrar>']
